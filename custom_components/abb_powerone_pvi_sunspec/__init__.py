1650c9c9b82267586ece3606b30eb50f13626847
Alessandro Del Prete <alessandro.delprete@gmail.com>
2023-02-20 12:32:08 +0100

    Fix Unload

--- custom_components/abb_powerone_pvi_sunspec/__init__.py
+++ custom_components/abb_powerone_pvi_sunspec/__init__.py
@@ -75,13 +75,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Handle removal of an entry"""
-    coordinator = hass.data[DOMAIN][entry.data["name"]]
     unloaded = all(
         await asyncio.gather(
             *[
                 hass.config_entries.async_forward_entry_unload(entry, component)
                 for component in PLATFORMS
-                if component in coordinator.platforms
             ]
         )
     )

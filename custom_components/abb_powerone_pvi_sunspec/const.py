7800a9d6a5eae8657641162a2959feb9a4ecc11b
Alessandro Del Prete <alessandro.delprete@gmail.com>
2023-08-07 14:57:57 +0200

    Added model UNO-DM-6.0-TL-PLUS

--- custom_components/abb_powerone_pvi_sunspec/const.py
+++ custom_components/abb_powerone_pvi_sunspec/const.py
@@ -204,6 +204,7 @@ DEVICE_STATUS = {
 DEVICE_MODEL = {
       1: "UNO-DM-4.0-TL-PLUS",
       3: "UNO-DM-4.6-TL-PLUS",
+      5: "UNO-DM-6.0-TL-PLUS",
      11: "UNO-DM-2.0-TL-PLUS",
      12: "UNO-DM-3.0-TL-PLUS",
      13: "REACT2-UNO-5.0-TL",

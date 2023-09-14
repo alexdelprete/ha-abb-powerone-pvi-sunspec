a36e012da26439f523f30d62fef16a4e4a86ca59
Alessandro Del Prete <alessandro.delprete@gmail.com>
2023-09-14 17:52:51 +0200

    Squeezed the reload config bug...

--- custom_components/abb_powerone_pvi_sunspec/config_flow.py
+++ custom_components/abb_powerone_pvi_sunspec/config_flow.py
@@ -172,9 +172,7 @@ class ABBPowerOnePVISunSpecOptionsFlow(config_entries.OptionsFlow):
             self.hass.config_entries.async_update_entry(
                 self.config_entry, data=user_input, options=self.config_entry.options
             )
-            # reload updated config entries (ht @fuatakgun)
-            # await self.hass.config_entries.async_reload(self.config_entry.entry_id)
-            # self.async_abort(reason="configuration updated")
+            self.async_abort(reason="configuration updated")
 
             # write empty options entries (ht @PeteRage / @fuatakgun)
             return self.async_create_entry(title="", data={})
